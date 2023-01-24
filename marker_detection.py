a4ae77b51bbef0cad7c166bbac052ae6d0f517c3
SpiderDerp <62308557+SpiderDerp@users.noreply.github.com>
2023-01-23 21:03:52 -0500

    detects perimeter markers

--- marker_detection.py
+++ marker_detection.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from pupil_apriltags import Detector
+import copy
 
 
 vid = cv2.VideoCapture(0)
@@ -15,15 +16,53 @@ at_detector = Detector(
    debug=0
 )
 
+def draw_tags(
+    image,
+    tags
+):
+    for tag in tags:
+        tag_family = tag.tag_family
+        tag_id = tag.tag_id
+        center = tag.center
+        corners = tag.corners
+
+        center = (int(center[0]), int(center[1]))
+        corner_01 = (int(corners[0][0]), int(corners[0][1]))
+        corner_02 = (int(corners[1][0]), int(corners[1][1]))
+        corner_03 = (int(corners[2][0]), int(corners[2][1]))
+        corner_04 = (int(corners[3][0]), int(corners[3][1]))
+
+        cv2.circle(image, (center[0], center[1]), 5, (0, 0, 255), 2)
+
+        cv2.line(image, (corner_01[0], corner_01[1]),
+                (corner_02[0], corner_02[1]), (255, 0, 0), 2)
+        cv2.line(image, (corner_02[0], corner_02[1]),
+                (corner_03[0], corner_03[1]), (255, 0, 0), 2)
+        cv2.line(image, (corner_03[0], corner_03[1]),
+                (corner_04[0], corner_04[1]), (0, 255, 0), 2)
+        cv2.line(image, (corner_04[0], corner_04[1]),
+                (corner_01[0], corner_01[1]), (0, 255, 0), 2)
+
+        cv2.putText(image, str(tag_id), (center[0] - 10, center[1] - 10),
+                   cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    return image
 
 while True:
-   ret, frame = vid.read()
-   cv2.imshow('frame', frame)
-   image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-   detections = at_detector.detect(image)
+   ret, image = vid.read()
 
-   cv2.imshow('frame', image)
+   debug_image = copy.deepcopy(image)
+   image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+   tags = at_detector.detect(
+      image,
+      estimate_tag_pose=False,
+      camera_params=None,
+      tag_size=None
+   )
+
+   debug_image = draw_tags(debug_image, tags)
+   cv2.imshow('frame', debug_image)
 
    if cv2.waitKey(1) & 0xFF == ord('q'):
       break
     
+
