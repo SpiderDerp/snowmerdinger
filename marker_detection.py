bc02ba25280fdbca5cfc3714cd6d06921bb7257a
SpiderDerp <62308557+SpiderDerp@users.noreply.github.com>
2023-01-24 18:11:26 -0500

    marker distance

--- marker_detection.py
+++ marker_detection.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from pupil_apriltags import Detector
 import copy
+import matplotlib.pyplot as plt 
 
 
 vid = cv2.VideoCapture(0)
@@ -25,11 +26,12 @@ def draw_tags(
         tag_id = tag.tag_id
         center = tag.center
         corners = tag.corners
+        dist = '%.2f'%tag.pose_t[2]
 
         center = (int(center[0]), int(center[1]))
         corner_01 = (int(corners[0][0]), int(corners[0][1]))
         corner_02 = (int(corners[1][0]), int(corners[1][1]))
-        corner_03 = (int(corners[2][0]), int(corners[2][1]))
+        corner_03 = (int(corners[2][0]), int(corners[2][1]))    
         corner_04 = (int(corners[3][0]), int(corners[3][1]))
 
         cv2.circle(image, (center[0], center[1]), 5, (0, 0, 255), 2)
@@ -43,26 +45,31 @@ def draw_tags(
         cv2.line(image, (corner_04[0], corner_04[1]),
                 (corner_01[0], corner_01[1]), (0, 255, 0), 2)
 
-        cv2.putText(image, str(tag_id), (center[0] - 10, center[1] - 10),
+        cv2.putText(image, f"{tag_id}", (center[0] - 10, center[1] - 10),
                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, f"{dist}", (center[0] - 30, center[1] - 30),
+                   cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
+        
+	
     return image
 
 while True:
-   ret, image = vid.read()
+	ret, image = vid.read()
 
-   debug_image = copy.deepcopy(image)
-   image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-   tags = at_detector.detect(
-      image,
-      estimate_tag_pose=False,
-      camera_params=None,
-      tag_size=None
-   )
+	debug_image = copy.deepcopy(image)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	tags = at_detector.detect(
+        image,
+        estimate_tag_pose=True,
+        camera_params= (640, 480, 1, 1),
+        tag_size=0.0508
+	)
 
-   debug_image = draw_tags(debug_image, tags)
-   cv2.imshow('frame', debug_image)
+	debug_image = draw_tags(debug_image, tags)
+	cv2.imshow('frame', debug_image)
 
-   if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
     
 
