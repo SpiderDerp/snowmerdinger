4b589471940ca6f4d3bf5cc0d9ccbf3a5c2d99d0
SpiderDerp <62308557+SpiderDerp@users.noreply.github.com>
2023-03-13 14:22:59 -0400

    joycon functionality added

--- motor-control/motor-control.py
+++ motor-control/motor-control.py
@@ -36,63 +36,41 @@ print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
 while(1):
-
-    x=raw_input()
-    
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         print("forward")
-         x='z'
-        else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         print("backward")
-         x='z'
-
-
-    elif x=='s':
+    if joycon.get_status()['analog-sticks']['right']['vertical'] == 1644 and joycon.get_status()['analog-sticks']['right']['horizontal'] == 2170:
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
-        x='z'
 
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
-
-    elif x=='b':
-        print("backward")
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.LOW)
+    elif joycon.get_status()['analog-sticks']['right']['vertical'] < 900:
+        print("down")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
 
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        x='z'
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.HIGH)
 
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        x='z'
+    elif joycon.get_status()['analog-sticks']['right']['vertical'] > 2000:
+        print("up")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
 
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        x='z'
-     
-    
-    elif x=='e':
-        GPIO.cleanup()
-        break
-    
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+        GPIO.output(in3, GPIO.HIGH)
+        GPIO.output(in4, GPIO.LOW)
+
+    elif joycon.get_status()['analog-sticks']['right']['horizontal'] < 900:
+        print("left")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+
+        GPIO.output(in3, GPIO.HIGH)
+        GPIO.output(in4, GPIO.LOW)
+
+    elif joycon.get_status()['analog-sticks']['right']['horizontal'] > 2000:
+        print("right")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.HIGH)
